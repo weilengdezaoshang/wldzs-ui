d8cd3eb028397d409b68494040714ca1db1022e0
chenGuoFeng <chenguofeng@xk.design>
2024-11-01 09:10:26 +0800

    feat: 自动部署

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'dumi';
 
+const repo = 'cool-ui';
+
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? `/${repo}` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
   outputPath: 'docs-dist',
+  history: {
+    type: 'hash',
+  },
   themeConfig: {
     name: 'cool-ui',
+    socialLinks: {
+      github: 'https://github.com/weilengdezaoshang/cool-ui',
+    },
+    nav: {
+      'zh-CN': [{ title: '组件', link: '/' }],
+      'en-US': [{ title: 'Blog', link: '/en/blog' }],
+    },
   },
 });
